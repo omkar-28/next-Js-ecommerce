ef41ff886b2c467a189024aae7ac70ddfe31dece
Omkar.Khatavkar <omkar.khatavkar@sinch.com>
2024-07-22 22:58:25 +0530

    Completed learning nextjs ecommerce

---
+++ src/email/PurchaseReceipt.tsx
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components"
+import { OrderInformation } from "./components/OrderInformation"
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string
+    imagePath: string
+    description: string
+  }
+  order: { id: string; createdAt: Date; pricePaidInCents: number }
+  downloadVerificationId: string
+}
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product name",
+    description: "Some description",
+    imagePath:
+      "/products/bd905e09-1505-4100-9884-b93db2bbc47b-4.png",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
